987dae7f962a0aaba4f1d93bc698c4410cd746e1
Tatiana Priemova <densatsu@mail.ru>
2023-03-15 20:22:48 +0400

    Lab 1 Part 1

---
+++ lab_1/scripts/build-client.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+PATH_TO_APP="../../../shop-angular-cloudfront"
+
+BUILD_OUTPUT_PATH="dist"
+BUILD_FILE=$BUILD_OUTPUT_PATH/client-app.zip
+
+cd $PATH_TO_APP
+
+if [[ ! "$(node -v)" =~ ^v14.20.*|^v16.13.*|^18.10.* ]]; then
+    echo "Error. Node.js version $(node -v) detected.";  
+    echo "The Angular CLI requires a minimum Node.js version of either v14.20, v16.13 or v18.10.";
+    exit;
+fi
+
+# exporting env variables
+export $(grep -v '^#' .env | xargs)
+
+echo 'Installing dependencies'
+npm i
+
+
+echo 'Building app'
+npm run build -- --configuration=$ENV_CONFIGURATION --output-path=$BUILD_OUTPUT_PATH/static/
+echo "Client app was built with $ENV_CONFIGURATION configuration."
+
+echo 'Zipping built files'
+[ -f "$BUILD_FILE" ] && rm $BUILD_FILE
+zip -r -j $BUILD_FILE $BUILD_OUTPUT_PATH/*
